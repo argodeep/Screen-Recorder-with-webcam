482f6113659fee3e9f05762ed969312e9d460619
Arghyadeep Majumder <arghya@skaas.guru>
2020-06-08 21:28:13 +0530

    Doc folder added

---
+++ docs/precache-manifest.a1bcdf587f71f9448ceae88b0e02caf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b16248a3c3c375aa1564311523699a",
+    "url": "/argodeep/index.html"
+  },
+  {
+    "revision": "29d23f147ff6fce23066",
+    "url": "/argodeep/static/css/main.3724f32b.chunk.css"
+  },
+  {
+    "revision": "157a7930f2ae87a2f7bb",
+    "url": "/argodeep/static/js/2.2a1d6032.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/argodeep/static/js/2.2a1d6032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d23f147ff6fce23066",
+    "url": "/argodeep/static/js/main.c3a4cce4.chunk.js"
+  },
+  {
+    "revision": "f80b64a0268a50038dcc",
+    "url": "/argodeep/static/js/runtime-main.142df3c4.js"
+  },
+  {
+    "revision": "2e6b892baac284d133c53ea0714cc4f6",
+    "url": "/argodeep/static/media/pause.2e6b892b.svg"
+  },
+  {
+    "revision": "a9759291e2ea751be46a486944c55bba",
+    "url": "/argodeep/static/media/play.a9759291.svg"
+  },
+  {
+    "revision": "664d473cbd4b902702d7dc466d8315e1",
+    "url": "/argodeep/static/media/stop.664d473c.svg"
+  }
+]);
