6991a18f1dcd0e158792739c5a905b01f4949bfe
argodeep <arghya@skaas.guru>
2020-06-08 21:40:20 +0530

    Fix: Build Error

---
+++ docs/precache-manifest.31721c73fc3580f72484ecbf0f586d01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc554689948b88192c481ef6a5bf831",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac31e599ee4b98f30599",
+    "url": "./static/css/main.3724f32b.chunk.css"
+  },
+  {
+    "revision": "157a7930f2ae87a2f7bb",
+    "url": "./static/js/2.2a1d6032.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2a1d6032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac31e599ee4b98f30599",
+    "url": "./static/js/main.ef19b59e.chunk.js"
+  },
+  {
+    "revision": "9ac7900e390d3ccc7c44",
+    "url": "./static/js/runtime-main.24ee5ed0.js"
+  },
+  {
+    "revision": "2e6b892baac284d133c53ea0714cc4f6",
+    "url": "./static/media/pause.2e6b892b.svg"
+  },
+  {
+    "revision": "a9759291e2ea751be46a486944c55bba",
+    "url": "./static/media/play.a9759291.svg"
+  },
+  {
+    "revision": "664d473cbd4b902702d7dc466d8315e1",
+    "url": "./static/media/stop.664d473c.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/argodeep/precache-manifest.a1bcdf587f71f9448ceae88b0e02caf1.js"
+  "./precache-manifest.31721c73fc3580f72484ecbf0f586d01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/argodeep/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
